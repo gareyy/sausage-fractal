7355fe2cfe4594ba839d02ea92b4204b7978d5cd
gareyyy <gk10guce@gmail.com>
2026-08-14 11:32:01 +1000

    Added multiple graphs and ability to generate multiple iterations of a curve

--- sausage.py
+++ sausage.py
@@ -1,9 +1,11 @@
 from typing import Callable
 import matplotlib.pyplot as plt
+from matplotlib.axes import Axes
 import numpy as np
 import torch
 from math import cos, sin, pi
 import time
+import concurrent.futures
 
 device = torch.get_default_device()
 rotcounter90 = torch.tensor([
@@ -21,6 +23,10 @@ def rotate(rotmat: torch.Tensor, to_rot: torch.Tensor, focal_point: torch.Tensor
     out = out.T.to(device)
     return out
 
+"""
+Scale factor of 3, Self similar objects = 5
+Hausdorff dimension of log 5/log 3 = 1.4649
+"""
 def quadratic_type_1(coord_list: torch.Tensor):
     start = coord_list[:, 0]
     end = coord_list[:, 1]
@@ -38,6 +44,10 @@ def quadratic_type_1(coord_list: torch.Tensor):
         )).to(device)
     return out
 
+"""
+Scale factor of 4, Self similar objects = 8
+Hausdorff dimension of log 8/log 4 = 1.5
+"""
 def sausage_curve(coord_list: torch.Tensor):
     start = coord_list[:, 0]
     end = coord_list[:, 1]
@@ -63,7 +73,10 @@ def sausage_curve(coord_list: torch.Tensor):
         )).to(device)
     return out
 
-
+"""
+Scale of 3, Self similar objects = 4
+Hausdorff dimension = log 4 / log 3 = 1.2618
+"""
 def snowflake_curve(coord_list: torch.Tensor) -> torch.Tensor:
     start = coord_list[:, 0]
     end = coord_list[:, 1]
@@ -86,15 +99,35 @@ def generate(start: torch.Tensor, num_iters: int = 1, curve: Callable[[torch.Ten
         out = curve(out)
     return out
 
+def generate_multiple(start: torch.Tensor, num_iters: int = 1, curve: Callable[[torch.Tensor], torch.Tensor] = sausage_curve) -> list[torch.Tensor]:
+    assert num_iters >= 1, "Number of iterations should be strictly positive"
+    outputs = []
+    iteration = start
+    for _ in range(num_iters):
+        iteration = curve(iteration)
+        outputs.append(torch.clone(iteration))
+    return outputs
+
+def draw_lines(lines: torch.Tensor, ax: Axes, title: str):
+    for i in lines:
+        ax.plot(i[:, 0], i[:, 1])
+    ax.set_aspect('equal')
+    ax.set_title(title)
+
+NUM_ITERS = 4
+
 if __name__ == "__main__":
     start = torch.tensor([
         [[0, 0], [1, 0]],
         ], dtype=torch.float32).to(device)
     begin = time.time()
-    out = generate(start, 5, curve=snowflake_curve)
+    out = generate_multiple(start, NUM_ITERS, curve=sausage_curve)
     end = time.time()
     print(f"{1000*(end-begin):.3f}ms")
-    for i in out:
-        plt.plot(i[:, 0], i[:, 1])
-    plt.gca().set_aspect('equal')
+    fig, ax = plt.subplots(3, 2)
+    fig.tight_layout()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=NUM_ITERS) as executor:
+        for it, lines in enumerate(out):
+            #draw_lines(lines, ax.flat[it], f"iteration {it+1}")
+            executor.submit(draw_lines, lines, ax.flat[it], f"iteration {it+1}")
     plt.show()
